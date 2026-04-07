cc838a798d8ca7935844443ed0e056364de948ee
Kenneth Lyngdoh <klyngdoh@gmail.com>
2026-04-07 10:14:06 +0000

    Removed unused

--- config/settings/local_example.py
+++ config/settings/local_example.py
@@ -1,12 +1,1 @@
 from config.settings.base import *
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db_name",
-        "USER": "user",
-        "PASSWORD": "pwd",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,1 @@
-import os
-
 from config.settings.base import *
-
-DEBUG = True if os.environ.get("DEBUG", "True") == "True" else False
